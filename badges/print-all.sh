d7db66e2aa0308c7a5f92c2a23658e87092b63d0
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2025-03-27 16:34:12 +0000

    parametrise

--- badges/print-all.sh
+++ badges/print-all.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 
+PRINTER=Brother1
+SRC=./badges
+OPTIONS="-o orientation-requested=5 -o PageSize=62x100mm -o Quality=High"
+
 # print all the the options
-lpoptions -p Brother1 -l
+lpoptions -p $PRINTER -l
 
 # print all the badges
-find -s ./badges -name "*.png" \
+find -s $SRC \
+    -name "*.png" \
     -exec echo "Printing" {} \; \
-    -exec lpr -P Brother1  -o orientation-requested=5 -o PageSize=62x100mm -o Quality=High {} \;
+    -exec lpr -P $PRINTER $OPTIONS {} \;
