e6515da017d8f961e19b624c6ac251631c89976c
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2025-03-27 16:28:53 +0000

    Print all the printer options && print badges in order

--- badges/print-all.sh
+++ badges/print-all.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
-find ./badges -name "*.png" \
+
+# print all the the options
+lpoptions -p Brother1 -l
+
+# print all the badges
+find -s ./badges -name "*.png" \
     -exec echo "Printing" {} \; \
     -exec lpr -P Brother1  -o orientation-requested=5 -o PageSize=62x100mm -o Quality=High {} \;
