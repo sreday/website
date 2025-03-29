f5e3e250645b7aa49fd2467d9141f4e539f3f913
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2025-03-29 02:42:17 +0000

    more comments

--- 2025-redmond-q2/_build/generate.py
+++ 2025-redmond-q2/_build/generate.py
@@ -28,7 +28,7 @@ def generate_talk_url(talk):
         title=("_" + talk.get("title", "").replace(",", "_").replace(" ", "_")) if talk.get("title") else "",
     )
     url = ''.join(filter(lambda x: x in string.printable, url))
-    url = re.sub('[\W]+', '', url)
+    url = re.sub('[\\W]+', '', url)
     return url[:100]
 
 def read_csv(path):
@@ -58,16 +58,10 @@ env.filters["markdown"] = lambda x: markdown.markdown(x)
 # load the context from the metadata file
 print(DIVIDER)
 print("Loading context")
+talks_raw = read_csv("./_db/talks.csv")
 with open('metadata.yml') as f:
     context = yaml.load(f, Loader=yaml.FullLoader)
     BASE_FOLDER = "./" + context.get("base_folder")
-
-
-try:
-    # read the csv
-    talks_raw = read_csv("./_db/talks.csv")
-except Exception as e:
-    print("Couldn't read talks", e)
 
 # pick up the ids & photos
 for i, talk in enumerate(talks_raw):
@@ -77,7 +71,6 @@ for i, talk in enumerate(talks_raw):
         talk["photo_url"] = "../speakers/" + photo
     else:
         talk["photo_url"] = talk.get("avatar")
-    print(generate_talk_url(talk))
     talk["short_url"] = generate_talk_url(talk)
 
 # sort into talks and keynotes
@@ -105,51 +98,27 @@ for talk in talks:
     tracks[track].append(talk)
 context["tracks"] = tracks_ordered
 
-# generate times
-def start_time():
-    return datetime.datetime(
-        hour=9,
-        minute=0,
-        year=2023,
-        month=9,
-        day=14,
-    )
-def close_time():
-    return datetime.datetime(
-        hour=17,
-        minute=0,
-        year=2023,
-        month=9,
-        day=14,
-    )
-
 # insert breaks
-first_block = context.get("block_sizes")[0]
+breaks = context.get("breaks")
 for track in tracks_ordered:
-    coffee = dict(
-        title="Coffee break",
-        duration=30,
-        comment="Main lobby",
-    )
-    lunch = dict(
-        title="Lunch & networking",
-        duration=60,
-        comment="Main lobby",
-    )
-    closing = dict(
-        title="Networking & sponsor crawl",
-        duration=30,
-        comment="Main lobby",
-    )
     talks = tracks[track]
-    tracks[track] = [coffee] + talks[:first_block] + [lunch] + talks[first_block:] + [closing]
+    offset = 0
+    tracks[track] = []
+    for brk in context.get("breaks"):
+        for i in range(brk.get("talks_before")):
+            tracks[track].append(talks[offset])
+            offset += 1
+        tracks[track].append(brk)
+    while offset < len(talks):
+        tracks[track].append(talks[offset])
+        offset += 1
 
 # prepend the first track each day with the keynotes
 # offset other tracks with that duration
 DEFAULT_DURATION = 30
 for i, track in enumerate(tracks_ordered):
-    current_day = (i // 3) + 1
-    current_time = start_time()
+    current_day = (i // len(context.get("rooms"))) + 1
+    current_time = datetime.datetime.fromisoformat(context.get("start_time"))
     prepend = []
     for talk in keynotes:
         if talk.get("day") == str(current_day):
@@ -164,15 +133,14 @@ for i, track in enumerate(tracks_ordered):
         talk["duration"] = int(talk.get("duration") or DEFAULT_DURATION)
         current_time += timedelta(minutes=talk["duration"])
     close = dict(
-        title="Venue closes & pub",
-        start_time=close_time(),
+        title="Wrap up",
+        start_time=current_time,
+        comment="Scan each other's QR codes & head to a nearby pub!"
     )
     tracks[track] = prepend + tracks[track] + [close]
 
 context["talks_by_tracks"] = tracks
 print("Loaded %d confirmed talks in %d tracks: %s" % (len(context["talks"]), len(tracks), tracks.keys()))
-
-
 
 # MAIN PAGES
 print(DIVIDER)
@@ -197,7 +165,7 @@ for talk in talks_raw:
 # SITEMAP
 print(DIVIDER)
 print("Generating sitemap.xml with %d items" % len(SITEMAP_URLS))
-now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+now = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=datetime.timezone.utc).isoformat()
 with open(BASE_FOLDER + "/sitemap.xml", "w") as f:
     template = env.get_template("sitemap.xml")
     f.write(template.render(urls=SITEMAP_URLS, now=now))
