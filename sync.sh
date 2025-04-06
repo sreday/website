663c7d89c012d02968f7755408f7ff290643a345
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2025-04-06 19:12:25 +0100

    add a simple script to update the event website with the template

---
+++ sync.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+set -o xtrace
+
+# usage:
+# ./sync.sh 2025-amsterdam-q4 --dry-run
+
+rsync -var \
+    --exclude "_db/*" \
+    --exclude "_assets/*" \
+    --exclude "_templates/venue.html" \
+    --exclude "metadata.yml" \
+    _event_template/ $1 $2
