55a4311cf9fdbbc3e96e226926a03c1cfc97602b
FranticAerobics <55240287+FranticAerobics@users.noreply.github.com>
2025-04-09 22:20:38 +0000

    Deploying to gh-pages from @ sreday/website@a8c3a6d5b1bf07c2bb3819ba6a9a570eeb518aa3 🚀

---
+++ 2025-london-q3/assets/js/main.js
@@ -0,0 +1,83 @@
+"use strict";
+
+/* ======= Header animation ======= */   
+const header = document.getElementById('header');  
+
+window.onload=function() 
+{   
+    headerAnimation(); 
+
+};
+
+window.onresize=function() 
+{   
+    headerAnimation(); 
+
+}; 
+
+window.onscroll=function() 
+{ 
+    headerAnimation(); 
+
+}; 
+    
+
+function headerAnimation () {
+
+    var scrollTop = window.scrollY;
+	
+	if ( scrollTop > 100 ) {	    
+	    header.classList.add('header-shrink');    
+	    	    
+	} else {
+	    header.classList.remove('header-shrink');
+	}
+
+};
+
+/* ===== Smooth scrolling ====== */
+/*  Note: You need to include smoothscroll.min.js (smooth scroll behavior polyfill) on the page to cover some browsers */
+/* Ref: https://github.com/iamdustan/smoothscroll */
+
+
+let scrollLinks = document.querySelectorAll('.scrollto');
+const pageNavWrapper = document.getElementById('navigation');
+
+scrollLinks.forEach((scrollLink) => {
+
+	scrollLink.addEventListener('click', (e) => {
+		
+		e.preventDefault();
+
+		let element = document.querySelector(scrollLink.getAttribute("href"));
+		
+		const yOffset = 69; //page .header height
+		
+		//console.log(yOffset);
+		
+		const y = element.getBoundingClientRect().top + window.pageYOffset - yOffset;
+		
+		window.scrollTo({top: y, behavior: 'smooth'});
+		
+		
+		//Collapse mobile menu after clicking
+		if (pageNavWrapper.classList.contains('show')){
+			pageNavWrapper.classList.remove('show');
+		}
+
+		
+    });
+	
+});
+    
+
+/* ===== Gumshoe SrollSpy ===== */
+/* Ref: https://github.com/cferdinandi/gumshoe  */
+// Get the sticky header
+
+
+// Initialize Gumshoe
+var spy = new Gumshoe('#navigation a', {
+	offset: 69 //page .header heights
+});
+
